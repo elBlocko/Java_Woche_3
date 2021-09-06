168e1b2af0cabd05d15c6f957abf66cd6c1ac442
Marco <marco.braeuer1984@gmail.com>
2021-09-06 09:42:19 +0200

    ueb20_2

--- src/uebung20/Rechner.java
+++ src/uebung20/Rechner.java
@@ -22,11 +22,12 @@ public class Rechner extends JFrame {
 	private JTextField tfOperandTwo;
 	private JTextField tfErg;
 	private String CerrMessage = "Die Eingaben sind unvollständig";
+
 	/**
-	 * JTextfield, JLabels  aus der toolbox
-	 * 4 JButtons aus der Toolbox, im Objektinspector umbenennen
-	 * Doppelklick auf Button erzeugt Click Event Handler
-	 * 2 Methoden allesAusgefuellt() und calc(string parameter) Übergabe aus dem Eventhandler
+	 * JTextfield, JLabels aus der toolbox 4 JButtons aus der Toolbox, im
+	 * Objektinspektor umbenennen Doppelklick auf Button erzeugt Click Event Handler
+	 * 2 Methoden allesAusgefuellt() und calc(string parameter) Übergabe aus dem
+	 * Eventhandler
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -52,39 +53,27 @@ public class Rechner extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		tfOperandOne = new JTextField();
-		tfOperandOne.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyPressed(KeyEvent e) {
-			
-			}
-		});
-		tfOperandOne.setToolTipText("");
-		tfOperandOne.setColumns(10);
-		tfOperandOne.setBounds(10, 47, 86, 20);
-		contentPane.add(tfOperandOne);
-		
-	
-		// EVENTHANDLER -------------------------------------------------------------------------
+
+		// EVENTHANDLER
+		// -------------------------------------------------------------------------
 		JButton btnPlus = new JButton("+");
 		btnPlus.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (allesAusgefuellt()) {					
+				if (allesAusgefuellt()) {
 					calc("plus");
 				} else {
 					tfErg.setText(CerrMessage);
 				}
-			
+
 			}
 		});
 		btnPlus.setBounds(100, 47, 48, 20);
 		contentPane.add(btnPlus);
-		
+
 		JButton btnMinus = new JButton("-");
 		btnMinus.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (allesAusgefuellt()) {					
+				if (allesAusgefuellt()) {
 					calc("minus"); // Parameter für die Methode
 				} else {
 					tfErg.setText(CerrMessage);
@@ -93,11 +82,11 @@ public class Rechner extends JFrame {
 		});
 		btnMinus.setBounds(158, 46, 48, 20);
 		contentPane.add(btnMinus);
-		
+
 		JButton btnDurch = new JButton("/");
 		btnDurch.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (allesAusgefuellt()) {					
+				if (allesAusgefuellt()) {
 					calc("durch");
 				} else {
 					tfErg.setText(CerrMessage);
@@ -106,11 +95,11 @@ public class Rechner extends JFrame {
 		});
 		btnDurch.setBounds(158, 78, 48, 20);
 		contentPane.add(btnDurch);
-		
+
 		JButton btnMal = new JButton("*");
 		btnMal.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (allesAusgefuellt()) {					
+				if (allesAusgefuellt()) {
 					calc("mal");
 				} else {
 					tfErg.setText(CerrMessage);
@@ -119,42 +108,88 @@ public class Rechner extends JFrame {
 		});
 		btnMal.setBounds(100, 79, 48, 20);
 		contentPane.add(btnMal);
-		
-		// ---------------------------------------------------------------------------------
-		
+
+		// zusätliche Events-------------------------------------------------------------------
+
 		tfOperandTwo = new JTextField();
+		tfOperandTwo.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_PLUS) {
+					if (allesAusgefuellt()) {
+						calc("plus");
+					} else {
+						tfErg.setText(CerrMessage);
+					}
+				}
+				if (e.getKeyCode() == KeyEvent.VK_MULTIPLY) {
+					if (allesAusgefuellt()) {
+						calc("mal");
+					} else {
+						tfErg.setText(CerrMessage);
+					}
+				}
+				if (e.getKeyCode() == KeyEvent.VK_SUBTRACT) {
+					if (allesAusgefuellt()) {
+						calc("minus");
+					} else {
+						tfErg.setText(CerrMessage);
+					}
+				}
+				if (e.getKeyCode() == KeyEvent.VK_DIVIDE) {
+					if (allesAusgefuellt()) {
+						calc("durch");
+					} else {
+						tfErg.setText(CerrMessage);
+					}
+				}
+
+			}
+		});
+		
+		// TextFields ---------------------------------------------------------------
+
+		tfOperandOne = new JTextField();	
+		tfOperandOne.setToolTipText("");
+		tfOperandOne.setColumns(10);
+		tfOperandOne.setBounds(10, 47, 86, 20);
+		contentPane.add(tfOperandOne);
+
+		
 		tfOperandTwo.setToolTipText("Operand 1");
 		tfOperandTwo.setColumns(10);
 		tfOperandTwo.setBounds(216, 47, 86, 20);
 		contentPane.add(tfOperandTwo);
-		
+
 		tfErg = new JTextField();
 		tfErg.setEditable(false);
 		tfErg.setToolTipText("Operand 1");
 		tfErg.setColumns(10);
 		tfErg.setBounds(10, 117, 292, 20);
 		contentPane.add(tfErg);
-		
-		// Labels ------------------------------------------------------------------------
-		
+
+		// Labels
+		// ------------------------------------------------------------------------
+
 		JLabel lblNewLabel = new JLabel("Operand 1");
 		lblNewLabel.setBounds(10, 25, 86, 14);
 		contentPane.add(lblNewLabel);
-		
+
 		JLabel lblOperand = new JLabel("Operand 2");
 		lblOperand.setBounds(216, 25, 86, 14);
 		contentPane.add(lblOperand);
-		
+
 		JLabel lblErgebnis = new JLabel("Ergebnis:");
 		lblErgebnis.setBounds(10, 96, 86, 14);
 		contentPane.add(lblErgebnis);
 
 	}
-	
-	// METHODEN ---------------------------------------------------------------------------
-	
+
+	// METHODEN
+	// ---------------------------------------------------------------------------
+
 	boolean allesAusgefuellt() {
-		if (tfOperandOne.getText().equals("") || tfOperandTwo.getText().equals("")){
+		if (tfOperandOne.getText().equals("") || tfOperandTwo.getText().equals("")) {
 			return false;
 		} else
 			return true;
@@ -165,33 +200,31 @@ public class Rechner extends JFrame {
 		operand1 = Double.parseDouble(tfOperandOne.getText().replace(",", "."));
 		operand2 = Double.parseDouble(tfOperandTwo.getText().replace(",", "."));
 		double erg = 0;
-		
+
 		if (send == "plus") {
-			erg = operand1 + operand2; 
-			
+			erg = operand1 + operand2;
+
 		}
 		if (send == "minus") {
-			erg = operand1 - operand2; 
-			
+			erg = operand1 - operand2;
+
 		}
 		if (send == "mal") {
-			erg = operand1 * operand2; 
-			
+			erg = operand1 * operand2;
+
 		}
-		if (send == "durch" && operand2 != 0) {			
-			erg = operand1 / operand2; 			
-		} else  {
+		if (send == "durch" && operand2 != 0) {
+			erg = operand1 / operand2;
+		} else {
 			tfErg.setText("Division durch Null ist nicht definiert");
-		    return; // return verlässt die Methode, vgl break verlässt die Anweisung
+			return; // return verlässt die Methode, vgl break verlässt die Anweisung
 		}
 		DecimalFormat f = new DecimalFormat("#0.00");
 		tfErg.setText(f.format(erg));
-		
-		
-		
+
 		tfOperandOne.requestFocus(); // tf erhält Fokus
 		tfOperandOne.selectAll(); // Markiert den Eintrag
 
 	}
-	
+
 }
